997132b988a8a74912e726b0623dee89cbc43c7d
Nikolas Tapia <tapia@wias-berlin.de>
2025-03-21 17:54:25 +0100

    coproduct examples

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ y = Tridend.vee(u, u)
 w = Tridend.vee(u, u, u)
 ly = Tridend.vee(u, y)
 yl = Tridend.vee(y, u)
+lyl = Tridend.vee(u, y, u)
 
 
 if __name__ == "__main__":
-    print(f"Shuffle: {y} ш {y} = {y@y}")
-    print(f"QShuffle: {y} * {y} = {y*y}")
+    print("Products:")
+    print(f"\tShuffle: {y} ш {y} = {y@y}")
+    print(f"\tQShuffle: {y} * {y} = {y*y}")
+
+    print("Coproducts:")
+    print(f"\tShuffle: Δ{lyl} = {lyl.coprod_sh()}")
+    print(f"\tQShuffle: Δ{lyl} = {lyl.coprod_qsh()}")
